389bd1d1e305e544bdc0b8f9e8e0d239f34f10ee
Deja Lazzarotto <deja@deja.com.br>
2020-08-18 11:29:53 -0300

    Adicionado a mensagem Como tem passado?

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace CursoCSharp
         {
             Console.WriteLine("Oi!");
             Console.WriteLine("Bom dia!");
+            Console.WriteLine("Como tem passado?");
 
             Console.ReadLine();
         }
